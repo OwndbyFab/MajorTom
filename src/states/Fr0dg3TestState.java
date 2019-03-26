46f3610f69ae8499707f4854aba3ccedbd475651
Markus Feit <markus.feit@cgm.com>
2019-03-26 20:09:38 +0100

    Collision

--- src/states/Fr0dg3TestState.java
+++ src/states/Fr0dg3TestState.java
@@ -2,12 +2,10 @@ package states;
 
 import assets.SpaceShipVector;
 import main.MajorTom;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.geom.Line;
-import org.newdawn.slick.geom.Polygon;
-import org.newdawn.slick.geom.Shape;
-import org.newdawn.slick.geom.Vector2f;
+import org.newdawn.slick.geom.*;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 import physic.Collision;
@@ -18,6 +16,8 @@ public class Fr0dg3TestState extends BasicGameState {
     private SpaceShipVector spaceShip;
     private Shape[] shapes;
     private Collision collision;
+    RoundedRectangle rectangle;
+
 
     @Override
     public int getID() {
@@ -26,25 +26,27 @@ public class Fr0dg3TestState extends BasicGameState {
 
     @Override
     public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) {
-        spaceShip = new SpaceShipVector(new Vector2f(100,100));
-        Shape collisionLine = new Line(200,200,300,250);
+        spaceShip = new SpaceShipVector(new Vector2f(100, 100));
+        Shape collisionLine = new Line(200, 200, 300, 250);
 
-        Shape polygon = new Polygon();
-        ((Polygon) polygon).addPoint(500,500);
-        ((Polygon) polygon).addPoint(600,500);
-        ((Polygon) polygon).addPoint(550,600);
+        /*Shape polygon = new Polygon();
+        ((Polygon) polygon).addPoint(500, 500);
+        ((Polygon) polygon).addPoint(600, 500);
+        ((Polygon) polygon).addPoint(550, 600);*/
 
         Shape border = new Polygon();
-        ((Polygon) border).addPoint(0,0);
-        ((Polygon) border).addPoint(MajorTom.WIDTH,0);
-        ((Polygon) border).addPoint(MajorTom.WIDTH,MajorTom.HEIGHT);
-        ((Polygon) border).addPoint(0,MajorTom.HEIGHT);
+        ((Polygon) border).addPoint(0, 0);
+        ((Polygon) border).addPoint(MajorTom.WIDTH, 0);
+        ((Polygon) border).addPoint(MajorTom.WIDTH, MajorTom.HEIGHT);
+        ((Polygon) border).addPoint(0, MajorTom.HEIGHT);
 
+        Shape roundedRect = new RoundedRectangle(400,400, 40, 20, 20);
 
 
         shapes = new Shape[3];
         shapes[0] = collisionLine;
-        shapes[1] = polygon;
+        /*shapes[1] = polygon;*/
+        shapes[1] = roundedRect;
         shapes[2] = border;
         collision = new Collision();
     }
@@ -60,8 +62,9 @@ public class Fr0dg3TestState extends BasicGameState {
 
     @Override
     public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) {
-        collision.detectCollision(spaceShip,shapes);
-
+        collision.detectCollision(spaceShip, shapes);
         spaceShip.update(gameContainer);
+
+
     }
 }
